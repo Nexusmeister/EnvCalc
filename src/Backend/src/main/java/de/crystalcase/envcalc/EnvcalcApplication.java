a204c6c89db5c5ec91119563c71fcae6d2c9fd2a
Viktor <viktorziegler99@live.de>
2021-04-29 21:23:50 +0200

    init

---
+++ src/Backend/src/main/java/de/crystalcase/envcalc/EnvcalcApplication.java
@@ -0,0 +1,13 @@
+package de.crystalcase.envcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvcalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvcalcApplication.class, args);
+	}
+
+}
---
+++ src/Backend/src/test/java/de/crystalcase/envcalc/EnvcalcApplicationTests.java
@@ -0,0 +1,13 @@
+package de.crystalcase.envcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvcalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
