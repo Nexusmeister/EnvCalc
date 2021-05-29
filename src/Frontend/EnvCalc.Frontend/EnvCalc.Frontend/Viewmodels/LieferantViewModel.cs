ae7b4c29d2c7a1921cf5fd2ec3a11aa8ef1a545b
Kaltenbach, Robin <robin@cskaltenbach.de>
2021-05-29 17:58:04 +0200

    Titel hinzugefügt

--- src/Frontend/EnvCalc.Frontend/EnvCalc.Frontend/Viewmodels/LieferantViewModel.cs
+++ src/Frontend/EnvCalc.Frontend/EnvCalc.Frontend/Viewmodels/LieferantViewModel.cs
@@ -4,6 +4,10 @@ namespace EnvCalc.Frontend.ViewModels
 {
     public class LieferantViewModel : ViewModelBase
     {
-        
+        /// <summary>
+        /// Gets the title of the view model.
+        /// </summary>
+        /// <value>The title.</value>
+        public override string Title => "Lieferantensicht";
     }
 }
