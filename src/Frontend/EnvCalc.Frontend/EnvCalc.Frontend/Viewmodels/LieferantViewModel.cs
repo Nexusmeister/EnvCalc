973702df6c9addf85d1c3f522b30f7f9f9299a89
Robin Kaltenbach <37147173+Nexusmeister@users.noreply.github.com>
2021-05-30 23:28:50 +0200

    Grundstruktur Frontend Client (#32)
    * Neues Views hinzugefügt
    
    * ViewModels hinzugefügt
    
    * Renaming Namespace
    
    * Neues ViewModel
    
    * Titel hinzugefügt
    
    * MainWindow TabControl eingebaut
    
    * CustomControls

---
+++ src/Frontend/EnvCalc.Frontend/EnvCalc.Frontend/Viewmodels/LieferantViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Catel.MVVM;
+
+namespace EnvCalc.Frontend.ViewModels
+{
+    public class LieferantViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Gets the title of the view model.
+        /// </summary>
+        /// <value>The title.</value>
+        public override string Title => "Lieferantensicht";
+    }
+}
---
+++ src/Frontend/EnvCalc.Frontend/EnvCalc.Frontend/Viewmodels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Catel.MVVM;
+
+namespace EnvCalc.Frontend.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        
+    }
+}
