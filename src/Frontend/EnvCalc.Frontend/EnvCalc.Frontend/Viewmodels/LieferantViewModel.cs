e252dcb9a98455c7db279669e21a58d9be5e32f4
Robin Kaltenbach <37147173+Nexusmeister@users.noreply.github.com>
2021-06-03 17:37:43 +0200

    View erweitert, damit Prozesse angezeigt  und geladen werden können (#36)
    * Neue Klasse für Prozessinformationen
    
    * Tabreihenfolge ändern
    
    * Menüleiste hinzugefügt
    
    * Auswahlliste für Prozesse eingebaut
    
    * Viewmodel erweitert

--- src/Frontend/EnvCalc.Frontend/EnvCalc.Frontend/Viewmodels/LieferantViewModel.cs
+++ src/Frontend/EnvCalc.Frontend/EnvCalc.Frontend/Viewmodels/LieferantViewModel.cs
@@ -1,4 +1,7 @@
-﻿using Catel.MVVM;
+﻿using System.Collections.ObjectModel;
+using Catel.Data;
+using Catel.MVVM;
+using EnvCalc.BusinessObjects;
 
 namespace EnvCalc.Frontend.ViewModels
 {
@@ -9,5 +12,32 @@ namespace EnvCalc.Frontend.ViewModels
         /// </summary>
         /// <value>The title.</value>
         public override string Title => "Lieferantensicht";
+
+        /// <summary>
+        /// Gets or sets whether the user has agreed to continue.
+        /// </summary>
+        public ObservableCollection<Prozess> ProzessListe
+        {
+            get => GetValue<ObservableCollection<Prozess>>(ProzessListeProperty);
+            set => SetValue(ProzessListeProperty, value);
+        }
+
+        /// <summary>
+        /// Register the UserAgreedToContinue property so it is known in the class.
+        /// </summary>
+        public static readonly PropertyData ProzessListeProperty = 
+            RegisterProperty(nameof(ProzessListe), typeof(ObservableCollection<Prozess>));
+
+        public LieferantViewModel()
+        {
+            ProzessListe = new ObservableCollection<Prozess>()
+            {
+                new Prozess()
+                {
+                    Id = "123123",
+                    Titel = "NeuerTest"
+                }
+            };
+        }
     }
 }
