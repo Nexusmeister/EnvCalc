84ad3ae8a3b347b6ecd76f67d824ad97dbd1195e
Kaltenbach, Robin <robin@cskaltenbach.de>
2021-06-09 18:18:03 +0200

    Converter hinzugefügt

---
+++ src/Frontend/EnvCalc.Frontend/EnvCalc.Frontend/CustomControls/Converter/BooleanToCollapsedVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using Catel.MVVM.Converters;
+
+namespace EnvCalc.Frontend.CustomControls.Converter
+{
+    public class BooleanToCollapsedVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //reverse conversion (false=>Visible, true=>collapsed) on any given parameter
+            var input = value != null && (null == parameter ? (bool)value : !(bool)value);
+            return input ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
