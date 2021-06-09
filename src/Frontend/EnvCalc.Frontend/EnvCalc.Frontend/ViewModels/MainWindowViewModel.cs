0ce27b7be0e5d3a72933f6fab6263d87f44ed575
Kaltenbach, Robin <robin@cskaltenbach.de>
2021-06-09 18:18:16 +0200

    Menü für Dev sichtbar machen

--- src/Frontend/EnvCalc.Frontend/EnvCalc.Frontend/ViewModels/MainWindowViewModel.cs
+++ src/Frontend/EnvCalc.Frontend/EnvCalc.Frontend/ViewModels/MainWindowViewModel.cs
@@ -2,7 +2,9 @@
 using System.Windows.Input;
 using Catel.Data;
 using Catel.MVVM;
+using EnvCalc.BusinessObjects.Enums;
 using EnvCalc.Frontend.Commands;
+using EnvCalc.Tools;
 
 namespace EnvCalc.Frontend.ViewModels
 {
@@ -16,15 +18,30 @@ namespace EnvCalc.Frontend.ViewModels
             set => SetValue(IconProperty, value);
         }
 
+        public bool IstDevMenuSichtbar
+        {
+            get => GetValue<bool>(IstDevMenuSichtbarProperty);
+            set => SetValue(IstDevMenuSichtbarProperty, value);
+        }
+
         public ICommand CloseCommand { get; set; }
         public Action Close { get; set; }
 
         public static readonly PropertyData IconProperty = RegisterProperty(nameof(IconPath), typeof(string));
 
+        public static readonly PropertyData IstDevMenuSichtbarProperty =
+            RegisterProperty(nameof(IstDevMenuSichtbar), typeof(bool));
+
         public MainWindowViewModel()
         {
-            IconPath = "Ressourcen/envCalc_icon.png";
             CloseCommand = new Command(SchliesseAnwendung);
+            Initialisiere();
+        }
+
+        private void Initialisiere()
+        {
+            IconPath = "Ressourcen/envCalc_icon.png";
+            IstDevMenuSichtbar = AppSettings.Betriebsmodus == Betriebsmodus.Debug;
         }
 
         private void SchliesseAnwendung()
