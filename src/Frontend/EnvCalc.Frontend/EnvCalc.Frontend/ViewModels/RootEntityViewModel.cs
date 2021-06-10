a028ff95f7fd3e33ca57dd8b051475494db80768
Kaltenbach, Robin <robin@cskaltenbach.de>
2021-06-10 14:59:36 +0200

    Converter eingebaut

--- src/Frontend/EnvCalc.Frontend/EnvCalc.Frontend/ViewModels/RootEntityViewModel.cs
+++ src/Frontend/EnvCalc.Frontend/EnvCalc.Frontend/ViewModels/RootEntityViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Data;
 using AsyncAwaitBestPractices.MVVM;
