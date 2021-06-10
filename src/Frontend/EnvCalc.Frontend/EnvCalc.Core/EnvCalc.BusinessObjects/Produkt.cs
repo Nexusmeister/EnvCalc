3d9e2f3a45676d9f6ca7d4d4b458f2b34e73ad72
Kaltenbach, Robin <robin@cskaltenbach.de>
2021-06-10 15:43:19 +0200

    Neue Klasse hinzugefügt

---
+++ src/Frontend/EnvCalc.Frontend/EnvCalc.Core/EnvCalc.BusinessObjects/Produkt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EnvCalc.BusinessObjects
+{
+    public class Produkt
+    {
+        public string Name { get; set; }
+        public string Beschreibung { get; set; }
+        public DateTime Erstellungsdatum { get; set; }
+    }
+}
