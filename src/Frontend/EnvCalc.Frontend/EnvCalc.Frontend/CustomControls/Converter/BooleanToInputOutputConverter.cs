51fc94d14eac7677e78ac54021806c0268f1fbc3
Robin Kaltenbach <37147173+Nexusmeister@users.noreply.github.com>
2021-06-10 15:00:46 +0200

    Es wird jetzt auch angezeigt, ob es sich um ein Input oder Output handelt (#79)
    * Neues Enum für Exchange
    
    * Converter angelegt
    
    * Converter eingebaut

---
+++ src/Frontend/EnvCalc.Frontend/EnvCalc.Frontend/CustomControls/Converter/BooleanToInputOutputConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Catel.MVVM.Converters;
+using EnvCalc.BusinessObjects.Enums;
+
+namespace EnvCalc.Frontend.CustomControls.Converter
+{
+    public class BooleanToInputOutputConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var input = value != null && (null == parameter ? (bool)value : !(bool)value);
+            var enumValue = input ? ExchangeRichtung.Input : ExchangeRichtung.Output;
+
+            var result = Enum.GetName(typeof(ExchangeRichtung), enumValue);
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
