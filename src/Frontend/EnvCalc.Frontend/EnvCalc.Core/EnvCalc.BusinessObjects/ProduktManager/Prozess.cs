1fc0442900e0e05d1301f01c3a43d3db11ea38fd
Kaltenbach, Robin <robin@cskaltenbach.de>
2021-06-11 17:06:49 +0200

    Modelerstellung für Webservice

---
+++ src/Frontend/EnvCalc.Frontend/EnvCalc.Core/EnvCalc.BusinessObjects/ProduktManager/Prozess.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace EnvCalc.BusinessObjects.ProduktManager
+{
+    public class Prozess
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Amplifier { get; set; }
+        public List<Exchange> Inputs { get; set; }
+        public List<Exchange> Outputs { get; set; }
+    }
+}
