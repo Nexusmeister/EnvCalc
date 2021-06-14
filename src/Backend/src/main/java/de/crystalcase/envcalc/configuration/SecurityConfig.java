d298eb19f7f1c9eda11c77a86f52bc01efed800c
Viktor <viktorziegler99@live.de>
2021-06-14 15:36:01 +0200

    added hardcoded basic auth

--- src/Backend/src/main/java/de/crystalcase/envcalc/configuration/SecurityConfig.java
+++ src/Backend/src/main/java/de/crystalcase/envcalc/configuration/SecurityConfig.java
@@ -1,7 +1,10 @@
 package de.crystalcase.envcalc.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,11 +15,23 @@ import org.springframework.stereotype.Component;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.
-                authorizeRequests().anyRequest().permitAll()
+                authorizeRequests().anyRequest().authenticated()
+            .and()
+                .httpBasic()
             .and()
                 .csrf().disable();
     }
+
+    @Autowired
+    public void  configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("envcalc")
+                .password("{noop}!op7N^9AhXmq4@OEuq^N39iu7ZKyHQ0FaAggjIZG")
+                .roles("ADMIN");
+    }
+
 }
